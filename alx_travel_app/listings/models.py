3f91fcb670ebf20cd84d6e23aae285023cd35131
pastorsheggs <bamidel692@gmail.com>
2025-07-04 14:12:59 +0000

    Delete alx_travel_app directory

--- alx_travel_app/listings/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Listing(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
--- alx_travel_app/listings/views.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-class TestView(APIView):
-    def get(self, request):
-        return Response({"message": "Hello from listings!"})
